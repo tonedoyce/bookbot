4d3ffb09b7492ffa72e8bb70c4e429540008fa36
tonedoyce <doyce@live.de>
2024-08-23 23:46:38 +0200

    report

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main(): 
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(f'Words:\n {count_words(text)}\n')
+    print_report(count_words(text), char_dict(text), book_path)
+
+""" print(f'Words:\n {count_words(text)}\n')
     print(f'Chars:\n {char_dict(text)}\n')
-    print("Thank you for using BookBot")
+    print("Thank you for using BookBot") """
+
+
 
 def char_dict(text):
     low_text = text.lower()
@@ -21,5 +25,12 @@ def count_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def print_report(words, chars, path):
+    print("---Begin report of " +path+ " ---")
+    print(f'{words} found in the document\n')
+    for key in chars:
+        print(f'The character {key} was found {chars[key]} times')
+    print("--- End report ---")
 
 main()
