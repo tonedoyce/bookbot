cdae998b06c85bbf98568c277f98681c1b542790
tonedoyce <doyce@live.de>
2024-08-23 23:04:17 +0200

    despaghettified main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    words = file_contents.split()
-    print(len(words))
+def main(): 
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(f'Words:\n {count_words(text)}\n')
+    print(f'Chars:\n {char_dict(text)}\n')
+    print("Thank you for using BookBot")
 
-    lowered_file = file_contents.lower()
-
-    charcount = dict()
-    for char in lowered_file:
-    
+def char_dict(text):
+    low_text = text.lower()
+    char_dict = dict()
+    for char in low_text:
         try:
-            charcount[char] += 1
+            char_dict[char] += 1
         except:
-            charcount[char] = 1
-    
-    print(charcount)
-        
+            char_dict[char] = 1
+    return char_dict
 
+def count_words(text):
+   return len(text.split())
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
-
